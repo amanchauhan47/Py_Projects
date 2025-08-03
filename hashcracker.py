0164ab7b55b6bd92f34243f3cbad7b1832f543a4
Aman Chauhan <118190337+amanchauhan47@users.noreply.github.com>
2025-08-03 17:07:25 +0530

    Update hashcracker.py

--- hashcracker.py
+++ hashcracker.py
@@ -17,8 +17,9 @@ Usage: hashcracker.py <hash_type> <hash_sum> <wordlist_path>
 Current Algorithm Supports:
     md5, sha1, sha224, sha256, sha512
 
-Example:
-    python3 {sys.argv[0]} md5 5f4dcc3b5aa765d61d8327deb882cf99 /path/wordlist.txt"""
+Examples:
+    python3 {sys.argv[0]} md5 5f4dcc3b5aa765d61d8327deb882cf99 /path/wordlist.txt
+    python3 {sys.argv[0]} sha1 hash.txt /path/wordlist.txt"""
 
 if len(sys.argv) != 4 :
     print(help_message)
@@ -28,19 +29,32 @@ mode = sys.argv[1].lower()  # md5
 hash2crack = sys.argv[2].lower()    # 0800fc577294c34e0b28ad2839435945
 wordlist = sys.argv[3]      # /usr/share/wordlist/rockyou.txt
 
+if mode not in ["md5", "sha1", "sha224", "sha256", "sha512"]:
+    print(f"Error: '{mode}' hash mode not supported.")
+
 if not os.path.exists(wordlist):
-    print(f"Error: '{wordlist}' does not exist.")
+    print(f"Error: '{wordlist}' file does not exist.")
     sys.exit()
 
+if hash2crack.endswith(".txt"):
+    if os.path.exists(hash2crack):
+        with open(hash2crack,"r") as f:
+            hash2crack = f.read()
+    else:
+        print(f"Error: '{hash2crack}' file does not exist.")
+        sys.exit()
+else:
+    print(f"Error: Please give your hash file in a valid '*.txt' format.")
+    sys.exit()
 try:
     with open(wordlist,"r",encoding="latin-1") as f:
         for text in f:
             text = text.strip()
             text_hash = get_hash(text, mode)
             if text_hash == hash2crack:
-                print(f"\nHash Cracked: '{text}'")
+                print(f"\nHash Cracked: '{text}'\n")
                 break
         else:
-            print("\nHash not found.")
+            print("\nHash not found.\n")
 except Exception as e:
     print(e)
